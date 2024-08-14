0340b7b25129d08a973eb832e5903c8831665c14
Efi <134388584+xxefi@users.noreply.github.com>
2024-08-15 02:46:56 +0400

    Update ProductsController.cs

--- Wolt.Server/WoltAPI/Controllers/ProductsController.cs
+++ Wolt.Server/WoltAPI/Controllers/ProductsController.cs
@@ -5,107 +5,106 @@ using WoltAPI.DataAccess;
 using WoltAPI.Dtos;
 using WoltAPI.Models;
 
-namespace WoltAPI.Controllers
+namespace WoltAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    private readonly WoltDbContext _context;
+
+    public ProductsController(WoltDbContext context) => _context = context;
+
+    [HttpGet("GetProducts")]
+    public async Task<IActionResult> GetProductsAsync([FromQuery] string? query, [FromQuery] string? sortBy, CancellationToken ct)
     {
-        private readonly WoltDbContext _context;
+        IQueryable<Products> productsQuery = _context.Products;
 
-        public ProductsController(WoltDbContext context) => _context = context;
+        if (!string.IsNullOrEmpty(query))
+            productsQuery = productsQuery
+                .Where(p => p.Name.Contains(query)
+                || p.Description.Contains(query));
 
-        [HttpGet("GetProducts")]
-        public async Task<IActionResult> GetProductsAsync([FromQuery] string? query, [FromQuery] string? sortBy, CancellationToken ct)
+        if (!string.IsNullOrEmpty(sortBy))
         {
-            IQueryable<Products> productsQuery = _context.Products;
+            bool isDescending = sortBy == "desc";
 
-            if (!string.IsNullOrEmpty(query))
-                productsQuery = productsQuery
-                    .Where(p => p.Name.Contains(query)
-                    || p.Description.Contains(query));
-
-            if (!string.IsNullOrEmpty(sortBy))
+            productsQuery = sortBy switch
             {
-                bool isDescending = sortBy == "desc";
-
-                productsQuery = sortBy switch
-                {
-                    "New" => isDescending ? productsQuery.OrderByDescending(p => p.Id) : productsQuery.OrderBy(p => p.Id),
-                    "Price Ascending" => productsQuery.OrderBy(p => p.Price),
-                    "Price Descending" => productsQuery.OrderByDescending(p => p.Price),
-                    _ => productsQuery
-                };
-                
-            }
-            var products = await productsQuery.ToListAsync(ct);
-
-            if (products == null)
-                return NotFound("No data found");
-
-            return Ok(products);
-        }
-
-        [HttpPost("AddProducts")]
-        public async Task<IActionResult> AddProductsAsync([FromBody] ProductsDto productsDto)
-        {
-            if (productsDto == null)
-                return BadRequest("Product data is null");
-
-            var product = new Products
-            {
-                Id = Guid.NewGuid(),
-                Name = productsDto.Name,
-                Description = productsDto.Description,
-                Price = productsDto.Price,
-                ImageSrc = productsDto.ImageSrc,
+                "New" => isDescending ? productsQuery.OrderByDescending(p => p.Id) : productsQuery.OrderBy(p => p.Id),
+                "Price Ascending" => productsQuery.OrderBy(p => p.Price),
+                "Price Descending" => productsQuery.OrderByDescending(p => p.Price),
+                _ => productsQuery
             };
-            await _context.Products.AddAsync(product);
-            await _context.SaveChangesAsync();
-
-            return Ok("Product successful added");
+            
         }
-        [HttpPut("EditImageSource")]
-        public async Task<IActionResult> EditImageProductAsync(Guid id, [FromBody] string imageSrc)
+        var products = await productsQuery.ToListAsync(ct);
+
+        if (products == null)
+            return NotFound("No data found");
+
+        return Ok(products);
+    }
+
+    [HttpPost("AddProducts")]
+    public async Task<IActionResult> AddProductsAsync([FromBody] ProductsDto productsDto)
+    {
+        if (productsDto == null)
+            return BadRequest("Product data is null");
+
+        var product = new Products
         {
-            if (imageSrc == null)
-                return BadRequest("Image Source is required");
+            Id = Guid.NewGuid(),
+            Name = productsDto.Name,
+            Description = productsDto.Description,
+            Price = productsDto.Price,
+            ImageSrc = productsDto.ImageSrc,
+        };
+        await _context.Products.AddAsync(product);
+        await _context.SaveChangesAsync();
 
-            var existingProduct = await _context.Products.FindAsync(id);
+        return Ok("Product successful added");
+    }
+    [HttpPut("EditImageSource")]
+    public async Task<IActionResult> EditImageProductAsync(Guid id, [FromBody] string imageSrc)
+    {
+        if (imageSrc == null)
+            return BadRequest("Image Source is required");
 
-            if (existingProduct == null)
-                return NotFound($"Product with Id '{id}' not found");
+        var existingProduct = await _context.Products.FindAsync(id);
 
-            try
-            {
-                existingProduct.ImageSrc = imageSrc;
+        if (existingProduct == null)
+            return NotFound($"Product with Id '{id}' not found");
 
-                await _context.SaveChangesAsync();
-                return Ok($"Product:" +
-                    $" {existingProduct.Name}\n" +
-                    $"Price: {existingProduct.Price}\n" +
-                    $"Image Source Changed");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        [HttpDelete("DeleteProduct")]
-        public async Task<IActionResult> DeleteProductAsync(Guid id)
+        try
         {
-            var existingProduct = await _context.Products.FindAsync(id);
+            existingProduct.ImageSrc = imageSrc;
 
-            if (existingProduct == null)
-                return NotFound($"Product with Id '{id}' not found");
-
-            _context.Products.Remove(existingProduct);
             await _context.SaveChangesAsync();
-
-            return Ok($"Deleted Product:" +
-                $" \nId: {existingProduct.Id}" +
-                $"\nName: {existingProduct.Name}");
+            return Ok($"Product:" +
+                $" {existingProduct.Name}\n" +
+                $"Price: {existingProduct.Price}\n" +
+                $"Image Source Changed");
         }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete("DeleteProduct")]
+    public async Task<IActionResult> DeleteProductAsync(Guid id)
+    {
+        var existingProduct = await _context.Products.FindAsync(id);
+
+        if (existingProduct == null)
+            return NotFound($"Product with Id '{id}' not found");
+
+        _context.Products.Remove(existingProduct);
+        await _context.SaveChangesAsync();
+
+        return Ok($"Deleted Product:" +
+            $" \nId: {existingProduct.Id}" +
+            $"\nName: {existingProduct.Name}");
     }
 }
